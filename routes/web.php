ee8e9709aabcec5ec943b6ab3cf1119d528de6ff
cedrix21 <ravelocedrix@gmail.com>
2025-06-17 13:58:27 +0800

    Activity 4 - Authentication (Login)

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::post('/students', [StudentController::class, 'store'])->name('students.st
 Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
 Route::put('/students/{id}', [StudentController::class, 'update'])->name('students.update');
 Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
